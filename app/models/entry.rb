13d345c1813965a5eed88be406f19f5ef2945717
Kas <kboyd@meazurelearning.com>
2023-04-17 13:16:38 -0700

    added validation to ensure negative numbers can't be added.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ApplicationRecord
 
     validates :calories, :proteins, :carbs, :fats, :meal_type, presence: true
+    validates_numericality_of :calories, :proteins, :carbs, :fats, :greater_than => -1
 
     def day
         self.created_at.strftime("%b %e, %Y")
