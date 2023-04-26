5f730e87fc275b18f0f2d5ca45e7143b587e53a5
Kas <kboyd@meazurelearning.com>
2023-04-25 19:48:10 -0700

    updated the archives controller to include goal in the index action method so that I can render both goals and entries in the same view.

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,5 +1,6 @@
 class ArchivesController < ApplicationController
   def index
     @entries = Entry.all.group_by(&:day)
+    @goals = Goal.all
   end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,5 @@
 class Goal < ApplicationRecord
+    def day
+        self.created_at.strftime("%b %e, %Y")
+    end
 end
